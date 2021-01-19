88e9d73ba5bbbc6ffc31062807c89e6f8ff1e170
kuldeep patel <kuldeepp89@gmail.com>
2021-01-19 23:49:10 +0530

    Add files via upload

--- src/app/modules/service-provider-details/service-provider-details.component.ts
+++ src/app/modules/service-provider-details/service-provider-details.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input, OnChanges } from '@angular/core';
 })
 export class ServiceProviderDetailsComponent implements OnInit {
 	
-	@Input() providerDetails: object;
+	@Input() providerDetails: any;
 
 	constructor() { }
 
